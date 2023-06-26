95c504eebc324908d51b2120d19c92db5d4417c0
Sophia <chiaki.katsuno@live.ca>
2023-06-26 14:42:54 -0700

    changes

--- assertEqual.js
+++ assertEqual.js
@@ -16,7 +16,7 @@ assertEqual(1, 1);
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + "=== " + expected + "✅✅✅");
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "✅✅✅");
   } else {
     console.log("🛑🛑🛑 Assertion Failed: " +actual + "!== " + expected + "🛑🛑🛑");
   }
