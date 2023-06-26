847ca16e177ed0aff39293121c7c7bbd042a3adc
Sophia <chiaki.katsuno@live.ca>
2023-06-26 00:43:43 -0700

    more changes

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,24 @@
+/*
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+*/
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + "=== " + expected + "✅✅✅");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " +actual + "!== " + expected + "🛑🛑🛑");
   }
 };
 
