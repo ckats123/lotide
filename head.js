477340b908ebbf10fe4283f2e2b2736c86614fba
Sophia <chiaki.katsuno@live.ca>
2023-07-23 20:33:38 -0700

    improved function of head.js

--- head.js
+++ head.js
@@ -1,18 +1,20 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "✅✅✅");
-    } else {
-      console.log("🛑🛑🛑 Assertion Failed: " +actual + "!== " + expected + "🛑🛑🛑");
-    }
-  };
-  
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-
-  const head = function(array) {
-    return array[0];
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
   }
+};
+  
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Create a function head that returns the first item in the array
+
+const head = function(array) {
+  return array[0];
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
