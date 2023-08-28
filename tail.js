8e08f975fd16293e4e4c7e75a8d42cb6dc3256c7
Sophia <chiaki.katsuno@live.ca>
2023-08-28 14:49:29 -0700

    refactored code for testing of tail, improved function of tail.js

--- tail.js
+++ tail.js
@@ -1,7 +1,15 @@
 const assertEqual = require("./assertEqual");
 
 const tail = function (array) {
-  return array.slice(1);
+  let newArray = [];
+  if (array.length === 0) {
+    return undefined;
+  } else if (array.length === 1) {
+    return array;
+  } else {
+    return array.slice(1);
+  }
+  return newArray;
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,38 @@
-// test/tailTest.js
+const assert = require("chai").assert;
 const tail = require("../tail");
 
+describe("#tail", () => {
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(
+      tail(["Yo Yo", "Lighthouse", "Labs"]).length,
+      2
+    );
+  });
+
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]).length, 2);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(
+      tail(["Hello", "Lighthouse", "Labs"])[0],
+      "Lighthouse"
+    );
+  });
+  it("returns 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(
+      tail(["Hello", "Lighthouse", "Labs"])[1],
+      "Labs"
+    );
+  });
+  it("returns 'Hi' for ['Hi']", () => {
+    assert.strictEqual(tail(["Hi"])[0], "Hi");
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(tail([]), undefined);
+  });
+});
+
+/*
 // Test Case of tail.js: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
@@ -15,3 +47,4 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 // Test Case 2 of tail.js: ...
 const result1 = tail(["Hi"]);
 assertEqual(result1[0], "Hi"); //ensure empty array is returned
+*/
