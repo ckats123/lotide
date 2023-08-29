d288458698431792931910f37041d115b110a9c8
Sophia <chiaki.katsuno@live.ca>
2023-08-29 10:52:44 -0700

    refatored function

--- countOnly.js
+++ countOnly.js
@@ -1,59 +1,48 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "✅✅✅");
-    } else {
-      console.log("🛑🛑🛑 Assertion Failed: " +actual + "!== " + expected + "🛑🛑🛑");
-    }
-  };
-  
-  /*
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  */
+const assertEqual = require("./assertEqual");
 
+// countOnly function: given an array and an object,
+//it will return an object containing counts of everything that the input
+//object listed.
+// for this case, items are limited to strings
 
-  // countOnly function: given an array and an object,
-  //it will return an object containing counts of everything that the input
-  //object listed.
-  // for this case, items are limited to strings
-  
-  // allItems: an array of strings that we need to look through
-  // itemsToCount: an object specifying what to count
-  
-  const countOnly = function(allItems, itemsToCount) {
-    let results = {};
-    for (const item of allItems) {
-      console.log(item);
-      if (itemsToCount[item]) {
-        if (results[item]) {
-          results[item] += 1;
-        }
-        else {
-          results[item] = 1;
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function (allItems, itemsToCount) {
+  let results = {};
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
     }
   }
   return results;
-}
-  
-  const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
-  
-  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-  
-  assertEqual(result1["Jason"], 1);
-  assertEqual(result1["Karima"], undefined);
-  assertEqual(result1["Fang"], 2);
-  assertEqual(result1["Agouhanna"], undefined);
+};
 
- 
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
