c819190ca185c053ae5295b016edccc38207464f
Sophia <chiaki.katsuno@live.ca>
2023-08-29 10:51:08 -0700

    refactored countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,30 +1,4 @@
-/*
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
-  }
-};
-s
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-*/
-
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "✅✅✅");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + "!== " + expected + "🛑🛑🛑");
-  }
-};
-  
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require("./assertEqual");
 
 /*
 The function should take in a sentence (as a string) and
@@ -34,7 +8,7 @@ For example, countLetters('LHL') should return results indicating
 that L appears twice, and H once.
 */
 
-const countLetters = function(sentence) {
+const countLetters = function (sentence) {
   let count = {};
   for (let letter of sentence) {
     if (letter !== " ") {
@@ -44,7 +18,8 @@ const countLetters = function(sentence) {
         count[letter] = 1;
       }
     }
-  } return count;
+  }
+  return count;
 };
 
 console.log(countLetters("assignment for lighthouse labs"));
