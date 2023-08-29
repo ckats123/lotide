b89b072206e7bca027b4e8b7f9cfc27de164b60c
Sophia <chiaki.katsuno@live.ca>
2023-08-29 10:55:54 -0700

    fixed syntax error

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,18 +1,18 @@
 const assertEqual = require("./assertEqual");
-};
-      
+
 // Instruction
 //Implement the function findKeyByValue which takes in an object
 //and a value. It should scan the object and return the first key
 //which contains the given value. If no key with that given value is found,
 //then it should return undefined.
 
-const findKeyByValue = function(object, value) {
+const findKeyByValue = function (object, value) {
   for (let key in object) {
     if (object[key] === value) {
       return key;
     }
-  } return undefined;
+  }
+  return undefined;
 };
 
 //Use assertEqual to write test cases for various scenarios.
@@ -21,8 +21,11 @@ const findKeyByValue = function(object, value) {
 const bestTVShowsByGenre = {
   sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama: "The Wire",
 };
-  
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(
+  findKeyByValue(bestTVShowsByGenre, "That '70s Show"),
+  undefined
+);
