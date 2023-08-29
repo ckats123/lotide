6d152d76239d8d1ad62e22f4fb3d15089d5b69d9
Sophia <chiaki.katsuno@live.ca>
2023-08-29 10:49:44 -0700

    removed unnecessary comments

--- flatten.js
+++ flatten.js
@@ -1,41 +1,30 @@
-/*
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+    console.log(
+      "✅✅✅ Assertion Passed: " +
+        actual +
+        " === " +
+        expected +
+        "✅✅✅"
+    );
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+    console.log(
+      "🛑🛑🛑 Assertion Failed: " +
+        actual +
+        "!== " +
+        expected +
+        "🛑🛑🛑"
+    );
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-*/
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "✅✅✅");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + "!== " + expected + "🛑🛑🛑");
-  }
-};
-  
-/*
-  // TEST CODE
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-*/
-
-/* Implement a function eqArrays which takes in two arrays and
-returns true or false, based on a perfect match. */
-
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   let result = true;
   if (array1.length !== array2.length) {
     result = false;
   } else {
     for (let i = 0; i < array1.length; i++) {
-    //console.log(array1[i], array2[i]);
+      //console.log(array1[i], array2[i]);
       if (array1[i] !== array2[i]) {
         result = false;
       }
@@ -44,62 +33,12 @@ const eqArrays = function(array1, array2) {
   return result;
 };
 
-/*
-// TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-*/
-
-/* Instruction:
-Use assertEqual to write test cases for various scenarios.
-
-The tests can be written below the definition of your function.
-Here's one example to get you started:
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS */
-
-/* Implement assertArraysEqual which will take in two arrays and
-console.log an appropriate message to the console.
-
-The message will be similar to that of assertEqual.
-In fact, you could refer to the code for assertEqual to help you implement
-this. Instead of doing a simple === check though, this assertion function
-will make use of your eqArrays function for array comparison.
-
-Warning
-Functions that do not return values and instead print out messages to the
-console are more difficult to test using assertions. This is because we
-cannot simply compare their return value to an expected value.
-
-For this reason, we will not be writing assertions (test code)
-for assertArraysEqual. That said, you can and should call the function
- below its declaration and visually ensure that the message printed
- to the console is accurate. */
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Assertion Passed: " + array1 + " === " + array2 + "✅✅✅");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + array1 + "!== " + array2 + "🛑🛑🛑");
-  }
-};
-
-/*
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
-*/
-
-
 /* Instruction: Create a function flatten which will
 take in an array containing elements including nested arrays of elements,
  and return a "flattened" version of the array.
  */
 
-const flatten = function(array) {
+const flatten = function (array) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
     if (Array.isArray(array[i])) {
