5efa0f0642f11fb380455d8ee7bfc94ec286528e
Sophia <chiaki.katsuno@live.ca>
2023-08-29 11:19:13 -0700

    removed unnecessary comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,6 @@
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
+
 // The function will return a "slice of the array with elements taken from the
 // beginning." It should keep going until the callback/predicate returns a truthy
 // value.
@@ -47,24 +50,3 @@ console.log(results2);
 --
 [ 'I\'ve', 'been', 'to', 'Hollywood' ]
 */
-
-
-// ToDo Later
-
-Testing
-Don't forget to test the function!
-
-Instruction
-Bring in our assertArraysEqual function (and associated eqArrays function) in order write some easy-to-read test cases.
-
-Since takeUntil returns arrays, the best assertion function to use would be assertArraysEqual to compare its return value against an expected array.
-
-Tips
-Question
-Does your takeUntil function need to loop through the entire array once the callback returns a truthy value? If not, how can we make it stop?
-
-Complete Assignment
-With the function implemented and satisfactory tests in place, we can push our work and mark this activity as completed.
-
-Instruction
-Commit and push the new file within your Lotide git repository.
