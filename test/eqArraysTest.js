24c0baf5b8520a661cd9705a740c3ec4f309e541
Sophia <chiaki.katsuno@live.ca>
2023-09-09 20:03:45 -0700

    fixed bugs

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,5 +1,28 @@
 // test/eqArrays.js
+const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
+const { assert } = require("chai");
+
+describe("#eqArrays", () => {
+  it("should return true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("should return false for [1, 2, 3] and [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("should return true for ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.strictEqual(
+      eqArrays(["1", "2", "3"], ["1", "2", "3"]),
+      true
+    );
+  });
+  it("should return false for ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.strictEqual(
+      eqArrays(["1", "2", "3"], ["1", "2", 3]),
+      false
+    );
+  });
+});
 
 // Test Case for eqArrays.js
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
